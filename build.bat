aa2d052b7709ee7fd7e63f43e0f2dd6f2331d20a
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:47:57 +0200

    Push Update 04/10/2021, 09:47:41

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/math_solver/Main.java&&jar cvmf ../manifest.mf ../build/math_solver.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="math_solver.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/math_solver.jar
-:end
+python build.py %*
